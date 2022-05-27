76d6cdbc49f1aa25345637787d8ef86fa72acb03
T ontheTop <tuanak691@gmail.com>
2022-05-27 18:01:46 +0700

    ss

--- src/pages/ProductDetailPage/FacebookItems.jsx
+++ src/pages/ProductDetailPage/FacebookItems.jsx
@@ -7,9 +7,9 @@ const IconFont = createFromIconfontCN({
     scriptUrl: '//at.alicdn.com/t/font_8d5l8fzk5b87iudi.js',
 });
 export const CommentPlugin = () => {
-    const location = useLocation()
-    const href = "https://leanhtuan.com" + location.pathname
-    console.log(href)
+    const url="https://datn-fe-flame.vercel.app"
+    const href=url+ useLocation().pathname
+    const href2=window.location.href
     return (
         <FacebookProvider appId="998182394159488">
             <Comments width="800" href={href} />
@@ -18,26 +18,13 @@ export const CommentPlugin = () => {
 }
 
 export const LikeButton = () => {
-    const location = useLocation().pathname
-    const href =encodeURIComponent("https://leanhtuan.com"+location)
+    const stringUrl="https://datn-fe-flame.vercel.app"
+    const href=encodeURIComponent(stringUrl+ useLocation().pathname)
+    const href2 =encodeURIComponent(window.location.href)
     const url=`https://www.facebook.com/plugins/like.php?href=${href}%2F&width=174&layout=button_count&action=like&size=large&share=true&height=28&appId=998182394159488`
-    console.log(url)
     return (
         <iframe src={url} width="174" height="28" style={{ border: "none", "overflow": "hidden" }}
             scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
     );
 
-}
-
-export class ShareButtonn extends React.Component {
-    render() {
-        return (
-            <FacebookProvider appId="998182394159488">
-                <ShareButton href="http://www.facebook.com" className='d-flex align-items-center btn-primary px-2 ms-3' >
-                    <span className='me-2'> Share</span>
-                    <IconFont type="icon-facebook" />
-                </ShareButton>
-            </FacebookProvider>
-        );
-    }
 }
