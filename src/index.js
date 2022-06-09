a210838f2711192e7ccbfa6ac44b34362076fe87
T ontheTop <tuanak691@gmail.com>
2022-06-09 23:32:02 +0700

    ss

--- src/index.js
+++ src/index.js
@@ -9,14 +9,16 @@ import { LoadingProvider } from '../src/context/LoadingProvider'
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const clientId = "597507392876-c941fph1dbiikn70madcsgfpunpbmlnn.apps.googleusercontent.com"
+//const clientId="39336608773-ttq6cf0bsq3ns1g1acsb3nq6a4cu50ac.apps.googleusercontent.com"
 root.render(
-  <LoadingProvider>
-    <Provider store={store}>
-      <BrowserRouter>
-        <GoogleOAuthProvider clientId="39336608773-ttq6cf0bsq3ns1g1acsb3nq6a4cu50ac.apps.googleusercontent.com">
+  <GoogleOAuthProvider GoogleOAuthProvider clientId={clientId} >
+    <LoadingProvider>
+      <Provider store={store}>
+        <BrowserRouter>
           <App></App>
-        </GoogleOAuthProvider>
-      </BrowserRouter>
-    </Provider>
-  </LoadingProvider>
+        </BrowserRouter>
+      </Provider>
+    </LoadingProvider>
+  </GoogleOAuthProvider>
 );
